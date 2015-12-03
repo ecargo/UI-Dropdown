ccb898714e48bc6c4337cba64f3756ad5cbcf328
Neaox <contact@chrisdyson.co.nz>
2015-12-04 09:49:17 +1300

    [Dropdown] Allow ignoring of case when getting an item
    Currently get.item compares labels and values case sensitively. This can cause duplicates when allowAdditions is true. To prevent these duplicates get.item should be capable of comparing labels and values while ignoring case.
    
    However the current behaviour should be preserved by default.
    
    To achieve this a new setting is added called ignoresCase, which when true will disregard the casing of labels and string values when performing the comparison.

--- dropdown.js
+++ dropdown.js
@@ -1853,6 +1853,10 @@ $.fn.dropdown = function(parameters) {
                 ? module.get.values()
                 : module.get.text()
             ;
+            value = (typeof value === "string" && settings.ignoresCase)
+              ? value.toLowerCase()
+              : value
+            ;
             shouldSearch = (isMultiple)
               ? (value.length > 0)
               : (value !== undefined && value !== null)
@@ -1869,6 +1873,14 @@ $.fn.dropdown = function(parameters) {
                     $choice       = $(this),
                     optionText    = module.get.choiceText($choice),
                     optionValue   = module.get.choiceValue($choice, optionText)
+                  ;
+                  optionText = (typeof optionText === "string" && settings.ignoresCase)
+                    ? optionText.toLowerCase()
+                    : optionText
+                  ;
+                  optionValue = (typeof optionValue === "string" && settings.ignoresCase)
+                    ? optionValue.toLowerCase()
+                    : optionValue
                   ;
                   // safe early exit
                   if(optionValue === null || optionValue === undefined) {
@@ -3537,6 +3549,8 @@ $.fn.dropdown.settings = {
   duration               : 200,        // duration of transition
 
   glyphWidth             : 1.037,      // widest glyph width in em (W is 1.037 em) used to calculate multiselect input width
+  
+  ignoresCase            : false,      // when determining if an item exists or not ignore case of value and label when comparing
 
   // label settings on multi-select
   label: {
